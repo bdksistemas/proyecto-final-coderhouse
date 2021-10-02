1b6e0754b7f24dd9819596f86c0014b083f099c6
AlejandroRdz <arodriguez@bdksistemas.com.mx>
2021-10-01 19:25:13 -0500

    Segunda entrega proyecto final

--- js/main.js
+++ js/main.js
@@ -1,22 +1,97 @@
 (function() {
     'use strict';
 
-    VanillaTilt.init(document.querySelectorAll(".destacados_card"), {
-        max: 20,
-        speed: 400,
-        glare: true,
-        "max-glare": 1,
-    });       
+    document.addEventListener('DOMContentLoaded', function(){
+
+        // CÓDIGO PARA ACTIVAR LA LIBRERÍA VANILLATILT, ANIMACIONES EN LAS CARD DEL INDEX.HTML
+        if (window.location.pathname == '/index.html') {
+            VanillaTilt.init(document.querySelectorAll(".destacados_card"), {
+                max: 20,
+                speed: 400,
+                glare: true,
+                "max-glare": 1,
+            });       
+        };
+        
+        document.addEventListener('click', (e) => {
+            e.preventDefault();
+
+            console.log(e.target);        
+
+            // BOTÓN PARA REPRODUCIR EL VÍDEO PROMOCIONAL
+            if ( e.target.matches('#play') || e.target.matches('#close') || e.target.matches('#imgPlay') ) {
+                toggleVideo();
+            }
+
+            // BOTÓN LATERAL PARA SUBIR AL INICIO DE LA PANTALLA
+            if ( e.target.matches('#botInicio') ) {
+                $('html, body').animate({
+                    scrollTop: 0
+                }, 600);        
+            }
+
+            // LINK A PAGINA DE INICIO
+            if ( e.target.matches('#msInicio') || e.target.matches('#miInicio')) {
+                $(location).attr('href','index.html');    
+            }            
+
+            // LINK A PAGINA DEL MENÚ 
+            if ( e.target.matches('#msMenu') || e.target.matches('#miMenu') || e.target.matches('#botMenu') || e.target.matches('#botSecMenu')) {
+                $(location).attr('href','menu.html');    
+            }            
+
+            // LINK A PPAGINA DE LAS PROMOCIONES
+            if ( e.target.matches('#msPromos') || e.target.matches('#miPromos') || e.target.matches('#botPromos')) {
+                $(location).attr('href','promos.html');    
+            }            
+
+            // LINK A PPAGINA DE LAS GALERIA
+            if ( e.target.matches('#msGaleria') || e.target.matches('#miGaleria') || e.target.matches('#botSecGaleria')) {
+                $(location).attr('href','galeria.html');    
+            }            
+
+            // LINK A PPAGINA DE LAS BLOG
+            if ( e.target.matches('#msBlog') || e.target.matches('#miBlog')) {
+                $(location).attr('href','blog.html');    
+            }            
+
+            // LINK A PPAGINA DE LAS CONTACTO
+            if ( e.target.matches('#msContacto') || e.target.matches('#miContacto') || e.target.matches('#botReservar')) {
+                $(location).attr('href','contacto.html');    
+            }            
+
+        });
+
+        // CÓDIGO PARA ACTIVAR EL SLIDER DE LAS RESEÑAS
+        if (window.location.pathname == '/index.html') {
+            var swiper = new Swiper(".reseñas_slider", {
+                spaceBetween: 20,
+                centeredSlides: true,
+                autoplay: {
+                    delay: 3500,
+                    disableOnInteraction: false,
+                },
+                loop: true,
+                breakpoints: {
+                    0: {
+                        slidesPerView: 1,
+                    },
+                    640: {
+                        slidesPerView: 2,
+                    },
+                    768: {
+                        slidesPerView: 2,
+                    },
+                    1024: {
+                        slidesPerView: 3,
+                    },                
+                }
+            });    
+        };
     
-    document.addEventListener('click', (e) => {
-        e.preventDefault();
-
-        if ( e.target.matches('#play') || e.target.matches('#close') || e.target.matches('#imgPlay') ) {
-            toggleVideo();
-        }
-
     });
 
+    // FUNCIÓN PARA ACTIVAR EL VIDEO Y REPRODUCIRLO AL ESTAR LA PANTALLA ACTIVA
     function toggleVideo() {
         const trailer = document.querySelector('.videoPopup');
         const video = document.querySelector('video');
